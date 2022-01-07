fa9c635ed6d0b97b310e7ecfd535ce10c289659b
yyy <1214858237@qq.com>
2022-01-08 00:39:49 +0800

    yyy

--- blog-server/src/main/java/com/yyy/blog/web/indexController.java
+++ blog-server/src/main/java/com/yyy/blog/web/indexController.java
@@ -1,19 +1,21 @@
 package com.yyy.blog.web;
 
-import com.yyy.blog.NotFoundException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class indexController {
 
-    @GetMapping("/")
-    public String index(){
+    @GetMapping("/{id}/{name}")
+    public String index(@PathVariable Integer id,
+                        @PathVariable String name){
 //        int a = 9 / 0;
-        String blog = null;
-        if(blog == null){
-            throw new NotFoundException("博客不存在");
-        }
+//        String blog = null;
+//        if(blog == null){
+//            throw new NotFoundException("博客不存在");
+//        }
+        System.out.println("------------index----------");
         return "index";
     }
 
